f50020c3479c2159eced9b78d0fa1394ddc63e75
Quincy Adams <qadams@fortissolutionsgroup.com>
2024-10-16 07:42:47 -0400

    Added Auto Deletion

--- src/Jobs/DeleteFileAfterExpiration.php
+++ src/Jobs/DeleteFileAfterExpiration.php
@@ -1,8 +1,32 @@
 <?php
 
-namespace Thinkneverland\Porter\Jobs;
+namespace ThinkNeverland\Porter\Jobs;
 
-class fdsg
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
+
+class DeleteFileAfterExpiration implements ShouldQueue
 {
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    protected $filePath;
+    protected $disk;
+
+    public function __construct(string $filePath, string $disk = 'public')
+    {
+        $this->filePath = $filePath;
+        $this->disk = $disk;
+    }
+
+    public function handle()
+    {
+        // Delete the file after the expiration time
+        if (Storage::disk($this->disk)->exists($this->filePath)) {
+            Storage::disk($this->disk)->delete($this->filePath);
+        }
+    }
 }
